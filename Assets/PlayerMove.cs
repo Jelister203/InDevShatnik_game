f5de691caf90a6865e0f67f98cfcddf4986747cf
zerad11 <iooin@bk.ru>
2022-10-17 23:37:27 +0300

    V1.2

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class PlayerMove : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
+        WallCheckRadiusUp = WallCheckUp.GetComponent<CircleCollider2D>().radius;
+        WallCheckRadiusDown = WallCheckDown.GetComponent<CircleCollider2D>().radius;
+        gravityDef = rb.gravityScale;
         realspeed = speed;
     }
 
@@ -22,7 +26,13 @@ public class PlayerMove : MonoBehaviour
         Jump();
         Run();
         CheckingGround();
-        
+        CheckingWall();
+        MoveOnWall();
+        WallJump();
+        Dash();
+        MaxSpeed();
+        Menu();
+        MilliAttack();
     }
     void CheckingGround()
     {
@@ -38,10 +48,19 @@ public class PlayerMove : MonoBehaviour
 
     void Walk()
     {
-        moveVector.x = Input.GetAxisRaw("Horizontal");
+        if (blockMoveX)
+        {
+            moveVector.x = 0;
+        }
+        else
+        {
+            moveVector.x = Input.GetAxisRaw("Horizontal");
+            rb.velocity = new Vector2(moveVector.x * realspeed, rb.velocity.y);
+        }
         anim.SetFloat("moveX", Mathf.Abs(moveVector.x));
-        rb.velocity = new Vector2(moveVector.x * realspeed, rb.velocity.y);
     }
+
+ 
 
     void Reflect()
     {
@@ -71,9 +90,9 @@ public class PlayerMove : MonoBehaviour
 
     void Jump()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if ((!blockMoveX)&&Input.GetKey(KeyCode.Space))
         {
-            if (OnGround) { jumpControl = true; }
+            if (!onWall && OnGround  ) { jumpControl = true; }
         }
         else { jumpControl = false; }
 
@@ -81,6 +100,8 @@ public class PlayerMove : MonoBehaviour
         {
             if (jumpIteration++ < jumpValueIteration)
             {
+                anim.StopPlayback();
+                anim.Play("Jump");
                 rb.AddForce(Vector2.up * jumpForce / jumpIteration);
             }
         }
@@ -93,7 +114,142 @@ public class PlayerMove : MonoBehaviour
     public LayerMask Ground;
 
 
+    public int DashImpulse = 10000;
+    void Dash()
+    {
+        if ((!blockMoveX)&&(!onWall) && (Input.GetKeyDown(KeyCode.LeftControl)))
+        {
+            anim.StopPlayback();
+            anim.Play("Dash");
+
+            rb.velocity = new Vector2(0, 0);
+            if (!faceRight) { rb.AddForce(Vector2.left * DashImpulse ); }
+            else { rb.AddForce(Vector2.right * DashImpulse ); } 
+        }
+    }
+
+    public bool onWall;
+    public LayerMask Wall;
+    public Transform WallCheckUp;
+    public Transform WallCheckDown;
+    public float WallCheckRayDistance = 1f;
+    private float WallCheckRadiusUp;
+    private float WallCheckRadiusDown;
+
+    void CheckingWall()
+    {
+        onWall = (Physics2D.OverlapCircle(WallCheckUp.position, WallCheckRadiusUp, Wall) && Physics2D.OverlapCircle(WallCheckDown.position, WallCheckRadiusDown, Wall));
+        anim.SetBool("onWall", onWall);
+    }
+
+    public float upDownSpeed = 4f;
+    public float slideSpeed = 0;
+    private float gravityDef;
+    void MoveOnWall()
+    {
+        if (onWall && !OnGround)
+        {
+            moveVector.y = Input.GetAxisRaw("Vertical");
+            anim.SetFloat("UpDown", moveVector.y);
+            if (!blockMoveX)
+            {
+                anim.StopPlayback();
+                anim.Play("UpDown");
+
+                if (moveVector.y == 0)
+                { 
+                    rb.gravityScale = 0;
+                    rb.velocity = new Vector2(0, slideSpeed);
+                }
+            }
+            
+            if (moveVector.y > 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, moveVector.y * upDownSpeed / 2);
+            }
+            else if (moveVector.y < 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, moveVector.y * upDownSpeed);
+            }
+            
+        }
+        else if (!OnGround && !onWall) { rb.gravityScale = gravityDef; }
+    }
+
+    private bool blockMoveX;
+    public float jumpWallTime = 0.5f;
+    private float timerJumpWall;
+    public Vector2 jumpAngle = new Vector2(3.5f, 10);
+    void WallJump()
+    {
+        if (onWall && !OnGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            blockMoveX = true;
+            moveVector.x = 0;
+            anim.StopPlayback();
+            anim.Play("Jump"); //вместо Jump будет WallJump
+            transform.localScale *= new Vector2(-1, 1);
+            faceRight = !faceRight;
+            rb.gravityScale = gravityDef;
+            rb.velocity = new Vector2(0, 0);
+            rb.velocity = new Vector2(transform.localScale.x * jumpAngle.x, jumpAngle.y);
+        }
+        if (blockMoveX && (timerJumpWall += Time.deltaTime) >= jumpWallTime)
+        {
+            if (onWall || OnGround || Input.GetKeyDown(KeyCode.LeftControl) || Input.GetAxisRaw("Horizontal") != 0)
+            {
+                blockMoveX = false;
+                timerJumpWall = 0;
+            }
+        }
+    }
+    
+    public float PlayerSpeed;
+    public int SpeedLimit = 10;
+    public int SpeedLimiter = 2;
+
+    void MaxSpeed()
+    {
+        PlayerSpeed = rb.velocity.magnitude;
+        if (!blockMoveX)
+        {
+            if (PlayerSpeed > SpeedLimit) { rb.velocity = new Vector2(moveVector.x / SpeedLimiter, rb.velocity.y / SpeedLimiter); }
+        }
+            
+    }
+    void Menu()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
+    public Transform MilliAttackArea;
+    public float MilliRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int AttackDamage = 40;
+    void MilliAttack()
+    {
+        if (OnGround&& !onWall && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            anim.SetTrigger("MilliAttack");
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(MilliAttackArea.position, MilliRange, enemyLayers);
+
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                enemy.GetComponent<enemy>().TakeDamage(AttackDamage);
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (MilliAttackArea == null) return;
+        Gizmos.DrawWireSphere(MilliAttackArea.position, MilliRange);
+    }
 }
+
 
 
 
